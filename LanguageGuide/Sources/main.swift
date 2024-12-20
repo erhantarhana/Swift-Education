9a9283340e1266ccef824e7a3ddf9ed34cdbd3c4
Erhan Tarhana <trhnerhan@gmail.com>
2024-12-20 11:30:16 +0300

    Added basic operators topic to main.swift

--- LanguageGuide/Sources/main.swift
+++ LanguageGuide/Sources/main.swift
@@ -15,3 +15,17 @@ func rentCar() {
 }
 
 rentCar()
+
+// Basic Operators
+var hourlyWage = 20.0 // Hourly wage in USD
+var regularHours = 150.0 // Standard working hours per month
+var overtimeHours = 25.0 // Overtime hours worked
+var overtimeRate = 1.5 // Overtime payment rate
+
+var regularPay = hourlyWage * regularHours
+var overtimePay = hourlyWage * overtimeRate * overtimeHours
+var totalPay = regularPay + overtimePay
+
+print("Regular pay: \(regularPay) USD")
+print("Overtime pay: \(overtimePay) USD")
+print("Total pay: \(totalPay) USD")
